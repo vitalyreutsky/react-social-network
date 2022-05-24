c13e53de1e8cd5d8ad08f29d5c0791e5c3fd0258
Vitaly Reutsky <vreutsky77@mail.ru>
2022-05-24 19:32:19 +0300

    Стилизовал компонент Dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,9 +1,57 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./Dialogs.css";
 
 const Dialogs = (props) => {
-  return <div className="dialogs">
-     
-  </div>;
+  return (
+    <div className="dialogs">
+      <div className="dialog-items person">
+        <div className="person-item">
+          <NavLink className="person-item__link" to="#">
+            Vitaly
+          </NavLink>
+        </div>
+        <div className="person-item">
+          <NavLink className="person-item__link" to="#">
+            Vitya
+          </NavLink>
+        </div>
+        <div className="person-item">
+          <NavLink className="person-item__link" to="#">
+            Anna
+          </NavLink>
+        </div>
+        <div className="person-item">
+          <NavLink className="person-item__link" to="#">
+            Katya
+          </NavLink>
+        </div>
+        <div className="person-item">
+          <NavLink className="person-item__link" to="#">
+            Tolya
+          </NavLink>
+        </div>
+      </div>
+      <div className="dialog-items message">
+        <div className="message-item">
+          <p className="message-text">Hi</p>
+        </div>
+        <div className="message-item">
+          <p className="message-text">How are you?</p>
+        </div>
+        <div className="message-item">
+          <p className="message-text">Go!</p>
+        </div>
+        <div className="message-item">
+          <p className="message-text">Nice to meet you</p>
+        </div>
+        <div className="message-item">
+          <p className="message-text">Great</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
